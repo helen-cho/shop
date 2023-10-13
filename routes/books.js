b7cbfe5488c23b3c66dae4975f9d87a8e5d82874
helen-cho <hd9536@daum.net>
2023-10-13 12:17:02 +0900

    ver04

--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', {title:'도서검색', pageName:'books/search.ejs'})
 });
 
+//도서검색결과저장
+router.post('/search/insert', function(req, res){
+    const title=req.body.title;
+    const authors=req.body.authors;
+    const price=req.body.price;
+    const publisher=req.body.publisher;
+    const image=req.body.thumbnail;
+    const contents=req.body.contents;
+    console.log(title,authors,price,publisher,image,contents);
+});
+
 module.exports = router;
