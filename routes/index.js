934cbfd6aab9ca8dba70e0c2af9b8595d445965b
helen-cho <hd9536@daum.net>
2023-10-17 12:31:00 +0900

    ver06

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '홈페이지', pageName:'home.ejs'});
 });
 
+
+//지역검색 페이지
+router.get('/search', function(req, res){
+  res.render('index', {title:'지역검색', pageName:'local/search.ejs'});
+})
 module.exports = router;
