793d02a6f3cc00c705de433b493993ce21ae6147
helen-cho <hd9536@daum.net>
2023-10-18 17:41:09 +0900

    ver07

--- routes/review.js
+++ routes/review.js
@@ -4,9 +4,19 @@ var db = require('../db');
 
 router.get('/list.json', function(req, res){  //테스트 localhot:3000/review/list.json?bid=118
     const bid=req.query.bid;
-    const sql='select * from review where bid=? order by rid desc limit 0, 5';
-    db.get().query(sql, [bid], function(err, rows){
+    const page=parseInt(req.query.page);
+    const start=(page-1)*5;
+    const sql='select * from review where bid=? order by rid desc limit ?, 5';
+    db.get().query(sql, [bid, start], function(err, rows){
         res.send(rows);
+    });
+});
+
+router.get("/count", function(req, res){ //테스트 localhost:3000/review/count?bid=118
+    const bid=req.query.bid;
+    const sql='select count(*) cnt from review where bid=?';
+    db.get().query(sql, [bid], function(err, rows){
+        res.send(rows[0].cnt.toString());
     });
 });
 
