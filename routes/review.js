a0afcfb275ba886a5cf265d57422affeb815473f
helen-cho <hd9536@daum.net>
2023-10-19 11:45:42 +0900

    ver08

--- routes/review.js
+++ routes/review.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-router.get('/list.json', function(req, res){  //테스트 localhot:3000/review/list.json?bid=118
+router.get('/list.json', function(req, res){  //테스트 localhot:3000/review/list.json?bid=118&page=1
     const bid=req.query.bid;
     const page=parseInt(req.query.page);
     const start=(page-1)*5;
-    const sql='select * from review where bid=? order by rid desc limit ?, 5';
+    const sql='select * from view_review where bid=? limit ?,5';
     db.get().query(sql, [bid, start], function(err, rows){
         res.send(rows);
     });
@@ -27,6 +27,15 @@ router.post("/insert", function(req, res){
     const contents=req.body.contents;
     const sql='insert into review(bid, uid, contents) values(?,?,?)';
     db.get().query(sql, [bid, uid, contents], function(err){
+        res.sendStatus(200);
+    });
+});
+
+//리뷰삭제
+router.post('/delete', function(req, res){
+    const rid=req.body.rid;
+    const sql='delete from review where rid=?';
+    db.get().query(sql, [rid], function(err){
         res.sendStatus(200);
     });
 });
