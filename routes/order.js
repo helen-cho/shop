77e30cb748a5331a26e903e936ad79d235b821b6
helen-cho <hd9536@daum.net>
2023-10-20 18:15:06 +0900

    ver09

--- routes/order.js
+++ routes/order.js
@@ -20,5 +20,26 @@ router.get("/cart.json", function(req, res){
             res.send({order, user:rows[0]});
         });
     });
-})
+});
+
+//주문자정보 입력
+router.post("/purchase/insert", function(req, res){
+    const uid=req.body.uid;
+    const rname=req.body.rname;
+    const rphone=req.body.rphone;
+    const raddress1=req.body.raddress1;
+    const raddress2=req.body.raddress2;
+    const sum=req.body.sum;
+    console.log(uid, rname, rphone, raddress1, raddress2, sum);
+    let sql='insert into purchase(uid,rname,rphone,raddress1,raddress2,sum) values(?,?,?,?,?,?)';
+    db.get().query(sql,[uid,rname,rphone,raddress1,raddress2,sum], function(err){
+        console.log('.............err1', err);
+        sql='select last_insert_id() last';
+        db.get().query(sql, function(err, rows){
+            console.log('.................err2', err);
+            res.send(rows[0].last.toString());
+        });
+    });
+});
+
 module.exports = router;
